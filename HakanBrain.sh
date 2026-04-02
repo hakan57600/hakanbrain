f906612817d7633ba87f106a93df8537d163522a
Hakan57600 <81087988+Hakan57600@users.noreply.github.com>
2026-04-02 16:46:51 +0300

    Force upload

---
+++ HakanBrain.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Hakan Brain - Linux/Android Calistirici
+echo "--------------------------------------------------"
+echo "          🚀 HAKAN BRAIN BASLATILIYOR...          "
+echo "--------------------------------------------------"
+
+# 1. Python Kontrolü
+if ! command -v python3 &> /dev/null
+then
+    echo "[!] Python3 bulunamadi! Kurulum yapiliyor..."
+    if command -v pkg &> /dev/null; then pkg install python -y; else sudo apt install python3 -y; fi
+fi
+
+# 2. Beyin Dosyası Güncelleme (GitHub'dan En Son Halini Çek)
+TOKEN="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"
+echo "[+] Merkez (GitHub) ile senkronize olunuyor..."
+curl -H "Authorization: token $TOKEN" -L "https://raw.githubusercontent.com/Hakan57600/hakanbrain/main/brain.py" -o "brain.py"
+
+# 3. Kütüphane Kontrolü
+pip3 install requests psutil --quiet
+
+# 4. Beyni Başlat
+echo "[+] Beyin Aktif!"
+python3 brain.py
+
+echo "--------------------------------------------------"
+echo "✅ Islem Tamamlandi. Kapatmak icin bir tusa basin."
+read -n 1
